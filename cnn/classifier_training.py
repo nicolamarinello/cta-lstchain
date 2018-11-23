58da2b75cc36b7be8ef687051bd80e222788c21e
Nicola Marinello <nicolamarinello94@gmail.com>
2018-11-23 10:39:40 +0100

    added count gammas protons

--- cnn/classifier_training.py
+++ cnn/classifier_training.py
@@ -61,6 +61,11 @@ if __name__ == "__main__":
     print('Building training generator...')
     training_generator = DataGenerator(h5files[0:n_train], batch_size=batch_size, shuffle=shuffle)
     print('Number of training batches: ' + str(len(training_generator)))
+    train_idxs = training_generator.get_indexes()
+    train_gammas = np.unique(train_idxs[:, 2], return_counts=True)[1][1]
+    train_protons = np.unique(train_idxs[:, 2], return_counts=True)[1][0]
+    print('Number of training gammas: ' + str(train_gammas))
+    print('Number of training protons: ' + str(train_protons))
 
     print('Building validation generator...')
     validation_generator = DataGenerator(h5files[n_train:], batch_size=batch_size, shuffle=shuffle)
