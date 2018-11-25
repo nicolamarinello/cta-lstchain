d5167de91b6c58c4c7c54f026ef99da779313864
Nicola Marinello <nicolamarinello94@gmail.com>
2018-11-25 01:24:21 +0100

    new architecture classifierv4

--- cnn/classifiers.py
+++ cnn/classifiers.py
@@ -106,45 +106,30 @@ class ClassifierV4:
 
     def get_model(self):
 
-        self.model.add(Conv2D(32, kernel_size=3, input_shape=(1, self.img_rows, self.img_cols),
+        self.model.add(Conv2D(64, kernel_size=3, input_shape=(1, self.img_rows, self.img_cols),
                               data_format='channels_first', activation='relu'))
-        self.model.add(Conv2D(32, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
-        self.model.add(Dropout(0.4))
-
-        self.model.add(Conv2D(32, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(Conv2D(32, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Dropout(0.4))
-
         self.model.add(Conv2D(64, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(Conv2D(64, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
-        self.model.add(Dropout(0.4))
-
-        self.model.add(Conv2D(64, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(Conv2D(64, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
-        self.model.add(Dropout(0.4))
-
         self.model.add(Conv2D(128, kernel_size=3, data_format='channels_first', activation='relu'))
         self.model.add(Conv2D(128, kernel_size=3, data_format='channels_first', activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
-        self.model.add(Dropout(0.4))
-
-        self.model.add(Conv2D(128, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(Conv2D(128, kernel_size=3, data_format='channels_first', activation='relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
-        self.model.add(Dropout(0.4))
+        self.model.add(Dropout(0.25))
 
         self.model.add(Conv2D(256, kernel_size=3, data_format='channels_first', activation='relu'))
+        self.model.add(Conv2D(256, kernel_size=3, data_format='channels_first', activation='relu'))
+        self.model.add(Conv2D(512, kernel_size=3, data_format='channels_first', activation='relu'))
+        self.model.add(Conv2D(512, kernel_size=3, data_format='channels_first', activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Conv2D(1024, kernel_size=3, data_format='channels_first', activation='relu'))
+        self.model.add(Conv2D(1024, kernel_size=3, data_format='channels_first', activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
         self.model.add(Dropout(0.4))
 
         self.model.add(Flatten())
-        self.model.add(Dense(256, activation="relu"))
+        self.model.add(Dense(2048, activation="relu"))
         self.model.add(Dropout(0.4))
-        self.model.add(Dense(128, activation="relu"))
+        self.model.add(Dense(1024, activation="relu"))
         self.model.add(Dropout(0.4))
         self.model.add(Dense(units=1, activation="sigmoid"))
 
