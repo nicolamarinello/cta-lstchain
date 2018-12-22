308c88f606dcd4458052c6772fa0981e5fe8f520
Nicola Marinello <nicolamarinello94@gmail.com>
2018-12-22 18:13:37 +0100

    lst_count count how many events inside one or more folders

---
+++ cnn/lst_count.py
@@ -0,0 +1,109 @@
+from multiprocessing import Process
+from os import listdir, remove
+from os.path import isfile, join
+import tables
+from tables.exceptions import HDF5ExtError, NoSuchNodeError
+import argparse
+import multiprocessing as mp
+
+
+def get_LST_data(data):
+
+    data_LST = data.root.LST
+
+    # LST data
+    LST_event_index = [x['event_index'] for x in data_LST.iterrows()]
+
+    return len(LST_event_index) - 1
+
+
+def chunkit(seq, num):
+
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+
+def worker(h5files, i, return_dict):
+
+    lengths = 0
+
+    for l, f in enumerate(h5files):
+        # get the data from the file
+        try:
+            data_p = tables.open_file(f)
+            LST_event_len = get_LST_data(data_p)
+
+            lengths += LST_event_len
+
+        except HDF5ExtError:
+
+            print('\nUnable to open file' + f)
+
+        except NoSuchNodeError:
+
+            print('\nThis file has a problem with the data structure: ' + f)
+
+    return_dict[i] = lengths
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--dirs', type=str, default='', nargs='+', help='Folder that contain .h5 files.', required=True)
+
+    FLAGS, unparsed = parser.parse_known_args()
+
+    ncpus = mp.cpu_count()
+    print("\nNumber of CPUs: " + str(ncpus))
+
+    # get all the parameters given by the command line
+    folders = FLAGS.dirs
+
+    print('Folders: ' + str(folders) + '\n')
+
+    # create a single big list containing the paths of all the files
+    all_files = []
+
+    for path in folders:
+        files = [join(path, f) for f in listdir(path) if (
+                isfile(join(path, f)) and f.endswith(".h5"))]
+        all_files = all_files + files
+
+    # print('Files: ' + '\n' + str(all_files) + '\n')
+
+    num_files = len(all_files)
+
+    processes = []
+
+    manager = mp.Manager()
+    return_dict = manager.dict()
+
+    if ncpus >= num_files:
+        print('ncpus >= num_files')
+        for i, f in enumerate(all_files):
+            p = Process(target=worker, args=([f], i, return_dict))
+            p.start()
+            processes.append(p)
+    else:
+        print('ncpus < num_files')
+        c = chunkit(all_files, ncpus)
+        for i, f in enumerate(c):
+            p = Process(target=worker, args=(f, i, return_dict))
+            p.start()
+            processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    print(return_dict)
+
+    print('Number of events: ' + str(sum(return_dict.values())))
