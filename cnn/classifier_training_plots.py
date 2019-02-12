6cd13a279b51e1675661f9f01e5add4fc9562aaf
Nicola Marinello <nicolamarinello94@gmail.com>
2019-02-12 10:59:00 +0100

    fixed plots on class_traing_plots

--- cnn/classifier_training_plots.py
+++ cnn/classifier_training_plots.py
@@ -1,35 +1,40 @@
-import pickle
 import argparse
+import os
+import pickle
+
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         '--log', type=str, default='', help='Training history file.', required=True)
+    parser.add_argument(
+        '--transparent', type=bool, default=False, help='Specify whether plots have to be transparent.', required=False)
 
     FLAGS, unparsed = parser.parse_known_args()
 
     filen = FLAGS.log
+    tran = FLAGS.transparent
+    folder = dirname1 = os.path.basename(filen)
 
+    # read data from training history file
     with open(filen, 'rb') as f:
         x = pickle.load(f)
         losses = x['losses']
         val_losses = x['val_losses']
         accuracy = x['accuracy']
         val_accuracy = x['val_accuracy']
-        precision = x['precision']
-        val_precision = x['val_precision']
-        recall = x['recall']
-        val_recall = x['val_recall']
+        lrs = x['lrs']
 
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
-
+    # get number of epochs
     epochs = range(1, len(losses) + 1)
+
+    # training loss & accuracy
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 
     ax = axs[0]
     ax.plot(epochs, losses)
@@ -40,21 +45,17 @@ if __name__ == "__main__":
 
     ax = axs[1]
     ax.plot(epochs, accuracy, label='Accuracy')
-    # ax.plot(epochs, precision, label='Precision')
-    # ax.plot(epochs, recall, label='Recall')
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Accuracy')
     ax.set_title('Training accuracy')
     ax.grid(True)
-    # ax.legend(loc='upper left', fancybox=True, framealpha=0.)
 
     fig.suptitle('Training history')
 
-    fig.savefig('classifier_training.png', transparent=False)
+    fig.savefig(folder + 'classifier_training.png', transparent=tran)
 
+    # validation loss & accuracy
     fig2, axs2 = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
-
-    epochs = range(1, len(val_losses)+1)
 
     ax = axs2[0]
     ax.plot(epochs, val_losses)
@@ -65,17 +66,51 @@ if __name__ == "__main__":
 
     ax = axs2[1]
     ax.plot(epochs, val_accuracy, label='Accuracy')
-    # ax.plot(epochs, val_precision, label='Precision')
-    # ax.plot(epochs, val_recall, label='Recall')
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Accuracy')
     ax.set_title('Validation accuracy')
     ax.grid(True)
-    # ax.legend(loc='upper left', fancybox=True, framealpha=0.)
 
     fig2.suptitle('Validation history')
 
-    fig2.savefig('classifier_validation.png', transparent=False)
+    fig2.savefig(folder + 'classifier_validation.png', transparent=tran)
+
+    # training + validation lxoss & accuracy
+    fig3, axs3 = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+    ax = axs3[0]
+    ax.plot(epochs, losses, label='Loss')
+    ax.plot(epochs, val_losses, label='Validation loss')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Loss [binary_crossentropy]')
+    ax.set_title('Training + validation loss')
+    ax.grid(True)
+    ax.legend(loc='upper left', fancybox=True, framealpha=0.)
+
+    ax = axs3[1]
+    ax.plot(epochs, accuracy, label='Accuracy')
+    ax.plot(epochs, val_accuracy, label='Validation accuracy')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Training + validation accuracy')
+    ax.grid(True)
+    ax.legend(loc='upper left', fancybox=True, framealpha=0.)
+
+    fig3.suptitle('Training history')
+
+    fig3.savefig(folder + 'classifier_train_val.png', transparent=tran)
+
+    # lr
+    fig3, axs3 = plt.subplots(nrows=1, ncols=1, figsize=(6.5, 5))
+
+    axs3.plot(epochs, lrs, label='Learning rate')
+    axs3.set_xlabel('Epoch')
+    axs3.set_ylabel('Learning rate')
+    axs3.set_title('Learning rate')
+    axs3.grid(True)
+
+    # fig3.suptitle('Training history')
+
+    fig3.savefig(folder + 'lr.png', transparent=tran)
 
     # plt.show()
-
